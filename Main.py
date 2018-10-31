c7787daf4fc138edbb284291fede4129cced45f5
Leonardo Sirino <leonardosirino@hotmail.com>
2018-10-31 19:17:17 -0300

    Preparação da otimizaçao do ponto de interface

--- Main.py
+++ Main.py
@@ -15,47 +15,23 @@ Locate.set_f(f)
 semiperimeter = Locate.SemiPerimeter
 Locate.SetVelocity(5)
 
-# Posição dos sensores
-Locate.StructuredSensorDistribution(
-    lines=2, sensorsInLine=3, x0=0, y0=height / 3, dx=(diameter * m.pi) / 3, dy=height / 3, aligned=False)
-Locate.StructuredSensorDistribution(
-    lines=1, sensorsInLine=2, x0=0, y0=-semiperimeter / 2, dx=(diameter * m.pi) / 2, dy=0, aligned=False)
-Locate.StructuredSensorDistribution(lines=1, sensorsInLine=2, x0=(
-    diameter * m.pi) / 4, y0=height + semiperimeter / 2, dx=(diameter * m.pi) / 2, dy=0, aligned=False)
+# Ponto no tampo
+Pcap = VesselPoint()
+Pcap.Xcord = 0.2 * diameter * m.pi
+Pcap.Ycord = height + 100
+Pcap.OnCap = True
+Pcao.Cap = "sup"
+
+# Ponto na parede
+Pwall = VesselPoint()
+Pwall.Xcord = 0.3 * diameter * m.pi
+Pwall.Ycord = height * 0.2
+Pwall.OnCap = False
+
 
 # Ponto de teste
 xp = diameter * m.pi * 0.9
-yp = 900
+yp = 1100
 
 t = Locate.returnDeltaT(xp, yp, [-1], 'geodesic')
-data = []
-i = 0
-for AT in t:
-    data.append((i, AT))
-    i += 1
-
-print("Posição verdadeira:")
-print("x: " + str(round(xp, 4)) +
-      " / y: " + str(round(yp, 4)))
-print("\n")
-
-t0 = time.time()
-
-x = Locate.simpleLocation(data)
-
-t1 = time.time()
-print("Localização simplificada:")
-print(x)
-print("Tempo decorrido: " + str(t1 - t0))
-print("\n")
-
-t0 = time.time()
-
-x = Locate.completeLocation(data)
-
-t1 = time.time()
-print("Localização completa:")
-print(x)
-print("Tempo decorrido: " + str(t1 - t0))
-
-# Locate.fCostMap(data)
+print(t)
